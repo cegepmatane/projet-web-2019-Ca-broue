2f6c4807e4f61479d44f207f1db75afd7a966070
abeauquel <beauquel.alexandre@gmail.com>
2019-01-30 12:59:20 -0500

    refactor : struture du projet

---
+++ src/microcabroue/commun/vue/vue-connexion.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xela
+ * Date: 30/01/19
+ * Time: 12:56 PM
+ */
---
+++ src/microcabroue/publique/vue/vue-accueil.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xela
+ * Date: 30/01/19
+ * Time: 12:54 PM
+ */
