c2c9f6082fe6da95295e0ae9c90e01c977a5f72c
abeauquel <beauquel.alexandre@gmail.com>
2019-01-31 13:21:21 -0500

    feat : redirection vers l'accueil

--- src/microcabroue/publique/vue/vue-accueil.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: xela
- * Date: 30/01/19
- * Time: 12:54 PM
- */
---
+++ src/microcabroue/publique/vue/vue-liste-goodies.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xela
+ * Date: 31/01/19
+ * Time: 12:34 PM
+ */
+
+echo "liste des googies";
