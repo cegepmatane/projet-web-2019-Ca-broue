ae4a3102b1d74d36484ab61c906df6f4fdceaee5
Sedrick <sedrickidd@hotmail.com>
2019-01-31 14:56:57 -0500

    feat : deuxième étape de l'inscription

--- src/microcabroue/publique/vue/vue-inscription.php
+++ src/microcabroue/publique/vue/vue-inscription.php
@@ -1,5 +1,5 @@
 <?php 
-
+require_once("../../commun/vue/fragment/haut-de-page.php");
 function afficherPremiereEtape()
 {?>
  <form>   
@@ -28,4 +28,45 @@ function afficherPremiereEtape()
 </form>
 <?php 
 }
+function afficherDeuxiemeEtape()
+{
+?>
+<form>   
+<!-- nom-->
+<div class="form-group">
+<label for="nom"></label>
+<input type="nom" class="form-control" id="nom">
+</div>
+<!-- Prenom -->
+<div class="form-group">
+<label for="prenom"></label>
+<input type="prenom" class="form-control" id="prenom">
+</div>
+<!-- Adresse -->
+<div class="form-group">
+<label for="adresse"></label>
+<input type="adresse" class="form-control" id="adresse">
+</div>
+<!-- code postal -->
+<div class="form-group">
+<label for="code-postal"></label>
+<input type="code-postal" class="form-control" id="code-postal">
+</div>
+<!-- vile -->
+<div class="form-group">
+<label for="ville"></label>
+<input type="ville" class="form-control" id="ville">
+</div>
+<!-- condition d'utilisation -->
+<div class="form-group form-check">
+<label class="form-check-label">
+<input class="form-check-input" type="checkbox"> J'accepte les <a href="#">conditiond d'utilisations
+<!-- finaliser -->
+<button type="submit" class="btn btn-primary">S'inscrire</button>
+</form>
+
+
+<?php
+}
+require_once("../../commun/vue/fragment/bas-de-page.php");
 ?>
