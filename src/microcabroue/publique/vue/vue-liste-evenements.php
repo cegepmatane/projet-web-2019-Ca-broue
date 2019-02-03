38908c66df864eff64f5815338b464a3498cb6e3
Sedrick <sedrickidd@hotmail.com>
2019-02-03 18:38:45 -0500

    feat : ajout de la base pour la page des évenements #20

---
+++ src/microcabroue/publique/vue/vue-liste-evenements.php
@@ -0,0 +1,13 @@
+<?php
+include_once "../../commun/vue/fragment/entete-fragment.php";
+include_once "../../commun/vue/fragment/pied-de-page-fragment.php";
+include_once "../../commun/vue/fragment/a-cote-fragment.php";
+
+
+$page = (object)[
+    //"style" => "evenement.css",
+    "titre" => "évenements", 
+]
+
+
+?>
