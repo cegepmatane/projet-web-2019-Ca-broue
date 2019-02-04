436c9fa9d76824ae7be64e18a6680da2971294ab
abeauquel <beauquel.alexandre@gmail.com>
2019-02-04 17:27:59 -0500

    feat : bibli d'icons pour le site

--- src/microcabroue/commun/vue/fragment/entete-fragment.php
+++ src/microcabroue/commun/vue/fragment/entete-fragment.php
@@ -23,6 +23,7 @@ function afficherEntete($page = null){
         </title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
         <link href="commun/decoration/style-commun.css" rel="stylesheet" type="text/css"/>
         <link href="../commun/decoration/style-commun.css" rel="stylesheet" type="text/css"/>
 
