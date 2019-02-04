77dd0068bd0772efb273fa0688cf5efe3ea64879
Unknown <1643216@cgmatane.qc.ca>
2019-02-03 23:16:22 -0500

    wip : template vue a propos #21

---
+++ src/microcabroue/publique/vue/vue-a-propos.php
@@ -0,0 +1,35 @@
+<?php
+    include_once("../../commun/vue/fragment/entete-fragment.php");
+    include_once("../../commun/vue/fragment/pied-de-page-fragment.php");
+
+    $page = (object)[
+        "titre" => "À propos de nous",
+        "titrePrincipal" => "À propos de nous",
+    ];
+
+    function afficherPage($page = null){
+        if(!is_object($page))
+            $page = (object)[];
+
+        afficherEntete($page);
+
+        ?>
+
+        <div>
+            <p>Mettre la mission ici !!!!</p>
+        </div>
+
+        <!-- faire une boucle pour tout les membres du projet -->
+        <div>
+            <img src="" alt="photo-du-membre">
+            <h3>Nom du membre</h3>
+            <p>Description du membre</p>
+        </div>
+
+        <?php
+
+        afficherPiedDePage($page);
+    }
+
+    afficherPage($page);
+?>
