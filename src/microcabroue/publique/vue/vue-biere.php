1da17f6952a9555d86b8de53589402b2cfff9d91
Alec <salamandeurs@gmail.com>
2019-02-03 20:24:07 -0500

    feat : début du squelette de la page bière #3

--- src/microcabroue/publique/vue/vue-biere.php
+++ src/microcabroue/publique/vue/vue-biere.php
@@ -1,2 +1,26 @@
 <?php
-echo"Bière";
+include_once "../../commun/vue/fragment/entete-fragment.php";
+include_once "../../commun/vue/fragment/pied-de-page-fragment.php";
+include_once "../../commun/vue/fragment/a-cote-fragment.php";
+
+
+$page = (object)
+[
+    "titre" => "Bière",
+    "titrePrincipal" => "Bière",
+    "itemMenuActif" => "bière", 
+];
+
+function afficherPage($page = null){
+
+	afficherEntete($page);
+?>	
+
+
+<<?php
+
+afficherPiedDePage($page);
+
+}
+
+afficherPage($page);
