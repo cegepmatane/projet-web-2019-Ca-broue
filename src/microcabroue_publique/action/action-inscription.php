87c4d96aac0b6df39dd5350ad2580f3e310df273
Sedrick <sedrickidd@hotmail.com>
2019-02-06 14:20:36 -0500

    feat : Integration du modele utilisateur dans l'inscription #7

--- src/microcabroue_publique/action/action-inscription.php
+++ src/microcabroue_publique/action/action-inscription.php
@@ -1,4 +1,14 @@
 <?php
+require_once("../../../microcabroue_com_commun/modele/Utilisateur.php");
+
+function ajouterPersonne($personne) {
+
+    //Ajouter la personne avec $personne dans la BD.
+    //Vide Pour l'instant
+   
+}
+
+$utilisateur = new Utilisateur((object) $_POST);
 
 if(isset($_POST["action-aller-premiere-etape"]) && $_POST["action-aller-premiere-etape"] == "naviguer")
 {
@@ -8,7 +18,7 @@ if(isset($_POST["action-aller-premiere-etape"]) && $_POST["action-aller-premiere
 }
 if(isset($_POST["action-aller-seconde-etape"]) && $_POST["action-aller-seconde-etape"] == "naviguer")
 {
-    if($_POST["confirmation-mot-de-passe"] == $_POST["mot-de-passe"] && $_POST["mot-de-passe"] != "" && $_POST["nom-utilisateur"] != "" && $_POST["email"] != "") 
+    if($_POST["confirmation-mot-de-passe"] == $_POST["mot_passe"] && $_POST["mot_passe"] != "" && $_POST["pseudo"] != "" && $_POST["mail"] != "") 
     {
         $page->isPremiereEtape = false;
         $page->isSecondeEtape = true;
@@ -22,10 +32,11 @@ if(isset($_POST["action-aller-seconde-etape"]) && $_POST["action-aller-seconde-e
 if(isset($_POST["action-inscrire"]) && $_POST["action-inscrire"] == "inscrire")
 {
     //echo "<script>alert(\"".."\")</script>"; 
-    if($_POST["nom"] != "" && $_POST["prenom"] != "" && $_POST["adresse"] != "" && $_POST["code-postal"] != "" && $_POST["ville"] != "" && $_POST['accepter-condition'] == "coche")
+    if($_POST["nom"] != "" && $_POST["prenom"] != "" && $_POST["adresse_postal"] != "" && $_POST["code_postal"] != "" && $_POST["ville"] != "" && $_POST['accepter-condition'] == "coche")
     {
         $page->isEnErreur = false;
-        echo "<script>alert(\"Inscription! WOOOOOOOOOOO\")</script>"; 
+        ajouterPersonne($utilisateur);
+        //echo "<script>alert(\"Inscription! WOOOOOOOOOOO\")</script>"; 
     }
     else
     {
@@ -35,5 +46,5 @@ if(isset($_POST["action-inscrire"]) && $_POST["action-inscrire"] == "inscrire")
         $page->isEnErreur = true;
     }
 }
-afficherPage($page);
+afficherPage($utilisateur ,$page);
 ?>
