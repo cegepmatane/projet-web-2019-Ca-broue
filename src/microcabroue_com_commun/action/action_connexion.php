2bf5483475b3639f6e7d828ae14e6baf658ae9a6
Florian <flo>
2019-02-06 15:10:25 -0500

    feat : prototype connexion et stockage en session

--- src/microcabroue_com_commun/action/action_connexion.php
+++ src/microcabroue_com_commun/action/action_connexion.php
@@ -9,6 +9,26 @@ $listeUtilisateur = $accesseurUtilisateur->recupererListeUtilisateur();
 
 if(isset($_POST["action-connexion"]) && $_POST["action-connexion"] == "connexion")
 {
-    echo "tata";
+
+    //Temporaire 
+
+    $listeUtilisateur = $accesseurUtilisateur->recupererListeUtilisateur();
+
+    for ($i=0; $i< sizeof($listeUtilisateur); $i++){
+        if($_POST["pseudo"] == $listeUtilisateur[$i]->getPseudo()){
+
+            if($_POST["mot_passe"] == $listeUtilisateur[$i]->getMot_passe()){
+                session_start();
+                $_SESSION['pseudo'] = $listeUtilisateur[$i]->getPseudo();
+                $_SESSION['id'] = $listeUtilisateur[$i]->getId();
+            }
+            else{
+                echo "mauvais MDP";
+            }
+        }
+        else {
+            echo "Utilisateur n'existe pas";
+        }
+    }
 }
 
