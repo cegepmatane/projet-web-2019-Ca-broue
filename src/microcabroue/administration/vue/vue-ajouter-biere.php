29d39e808d9c95d9a5ccf369ead0114496a24bab
Mezeeh <1643216@cgmatane.qc.ca>
2019-02-07 14:29:03 -0500

    refactor : utilisation fragment formulaire pour ajouter biere #17

--- src/microcabroue/administration/vue/vue-ajouter-biere.php
+++ src/microcabroue/administration/vue/vue-ajouter-biere.php
@@ -1,6 +1,6 @@
 <?php
     include_once "fragment/entete-fragment.php";
-    include_once "fragment/modification-fragment.php";
+    include_once "fragment/formulaire-fragment.php";
     include_once "fragment/pied-de-page-fragment.php";
 
     $page = (object)
@@ -10,15 +10,7 @@
         "titrePrincipal" => "Panneau d'adminstration - Ça Broue !",
         "type" => "biere",
         "action" => "ajout",
-        "urlRetour" => "./vue-liste-bieres.php",
-        "listeChamps" => [
-            "labelMarque" => '<label for="marque">Marque de la bière</label>',
-            "champMarque" => "<input name='marque' id='marque-ajout-biere' type='text'>",
-            "labelDescription" => '<label for="description">Description de la bière</label>',
-            "champDescription" => "<textarea name='description' id='description-ajout-biere' cols='30' rows='10'></textarea>",
-            "labelPourcentageAlcool" => '<label for="pourcentage">Pourcentage d\'alcool de la bière</label>',
-            "champPourcentageAlcool" => '<input type="text" name="pourcentage" id="pourcentage-alcool-ajout-biere">'
-        ]
+        "urlRetour" => "./vue-liste-bieres.php"
     ];
     
     function afficherPage($page = null){
@@ -26,10 +18,9 @@
             $page = (object)[];
         
         afficherEntete($page);
-
-        afficherChamps($page);
+        afficherFormulaire($page);
+        afficherPiedDePage($page);
     }
 
     afficherPage($page);
-    afficherPiedDePage($page);
 ?>
