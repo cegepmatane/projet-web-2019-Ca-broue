f6078aff8ccdbc6c13e213af2d8f2ca2e990e946
Mezeeh <1643216@cgmatane.qc.ca>
2019-02-06 23:58:19 -0500

    feat : [NO CSS] footer panneau admin #17

---
+++ src/microcabroue/administration/vue/fragment/pied-de-page-fragment.php
@@ -0,0 +1,15 @@
+<?php
+
+    function afficherPiedDePage($page = null){
+        if(!is_object($page)) 
+            $page = (object)[];
+
+        ?>
+        
+        </body>
+        </html>
+
+        <?php
+    }
+
+?>
