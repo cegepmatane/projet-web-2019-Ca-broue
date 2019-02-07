6b04ea657268e6f5e5d63999a9651fc281da1d19
Mezeeh <1643216@cgmatane.qc.ca>
2019-02-07 00:01:51 -0500

    feat : template liste des goodies #17

---
+++ src/microcabroue/administration/vue/vue-liste-goodies.php
@@ -0,0 +1,34 @@
+<?php
+    include_once "fragment/entete-fragment.php";
+    include_once "fragment/liste-fragment.php";
+    include_once "fragment/pied-de-page-fragment.php";
+
+    $page = (object)
+    [
+        "titre" => "Gestion des goodies",
+        "titrePrincipal" => "Panneau d'adminstration - Ça Broue !",
+        "type" => "goodie",
+        "liste" => [
+            $goodie = (object)[
+                "nom" => "Goodie",
+                "id" => "0"
+            ],
+            $goodie = (object)[
+                "nom" => "Goodie2",
+                "id" => "1"
+            ]
+        ]
+    ];
+
+    function afficherPage($page = null){
+        if(!is_object($page))
+            $page = (object)[];
+        
+        afficherEntete($page);
+
+        afficherListe($page);
+    }
+
+    afficherPage($page);
+    afficherPiedDePage($page);
+?>
