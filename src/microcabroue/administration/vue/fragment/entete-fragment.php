e636bbe12a42d6d8a39f205287e26f7ef0457fbb
Mezeeh <1643216@cgmatane.qc.ca>
2019-02-06 23:57:10 -0500

    feat : [NO CSS] entete panneau admin #17

---
+++ src/microcabroue/administration/vue/fragment/entete-fragment.php
@@ -0,0 +1,43 @@
+<?php
+    function afficherEntete($page = null){
+        if(!is_object($page)) 
+            return;
+
+        ?>
+
+        <!DOCTYPE html>
+        <html lang="fr">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title><?= $page->titre ?? ""; ?></title>
+
+            <?php
+                if(isset($page->style)){
+                    ?>
+        
+                    <link href="<?= $page->style; ?>" rel="stylesheet" media="all"  type="text/css">
+        
+                    <?php
+                }
+            ?>
+        </head>
+        <body>
+            <header>
+                <ul>
+                    <li>
+                        <a href="./vue-liste-bieres.php">Bieres</a>
+                    </li>
+                    <li>
+                        <a href="./vue-liste-goodies.php">Goodies</a>
+                    </li>
+                    <li>
+                        <a href="./vue-liste-utilisateurs.php">Utilisateurs</a>
+                    </li>
+                </ul>
+            </header>
+
+
+        <?php
+    }
+?>
