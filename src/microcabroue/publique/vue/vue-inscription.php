8da72a4f56279bad1d0c4ebd290f00e976ed3679
Sedrick <sedrickidd@hotmail.com>
2019-02-07 13:37:43 -0500

    Formatage du code inscription #7

--- src/microcabroue/publique/vue/vue-inscription.php
+++ src/microcabroue/publique/vue/vue-inscription.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once("../../commun/vue/fragment/entete-fragment.php");
 require_once("../../commun/vue/fragment/pied-de-page-fragment.php");
 require_once("../../../microcabroue_com_commun/modele/Utilisateur.php");
@@ -14,106 +14,119 @@ $page = (object)
 ];
 
 function afficherPremiereEtape($utilisateur, $page = null)
-{?>
+{
+    ?>
     <div class="conteneur-inscription">
-        <form method="post">    
-        <!-- nom utilisateur -->
-        <div class="groupe-formulaire">
-            <label for="nom-utilisateur">Nom d'utilisateur</label>
-            <input class="controle-formulaire" id="pseudo" name="pseudo" value=<?=$utilisateur->getPseudo(); ?>>
-        </div>
-    <!-- addresse -->
-    <div class="groupe-formulaire">
-        <label for="email">Adresse mail</label>
-        <input type="email" class="controle-formulaire" id="email" name="mail" value=<?=$utilisateur->getMail(); ?>>
+        <form method="post">
+            <!-- nom utilisateur -->
+            <div class="groupe-formulaire">
+                <label for="nom-utilisateur">Nom d'utilisateur</label>
+                <input class="controle-formulaire" id="pseudo" name="pseudo" value=<?= $utilisateur->getPseudo(); ?>>
+            </div>
+            <!-- addresse -->
+            <div class="groupe-formulaire">
+                <label for="email">Adresse mail</label>
+                <input type="email" class="controle-formulaire" id="email" name="mail"
+                       value=<?= $utilisateur->getMail(); ?>>
+            </div>
+            <!-- Mot de passe -->
+            <div class="groupe-formulaire">
+                <label for="mot-de-passe">Mot de passe</label>
+                <input type="password" class="controle-formulaire" id="mot-de-passe" name="mot_passe">
+            </div>
+            <!-- confirmer Mot de passe -->
+            <div class="groupe-formulaire">
+                <label for="confirmation-mot-de-passe">Confirmer le mot de passe</label>
+                <input type="password" class="controle-formulaire" id="confirmation-mot-de-passe"
+                       name="confirmation-mot-de-passe">
+            </div>
+            <!-- Prochaine etape -->
+            <button type="submit" class="bouton bouton-primaire" name="action-aller-seconde-etape" value="naviguer">
+                Poursuivre
+            </button>
+        </form>
     </div>
-    <!-- Mot de passe -->
-    <div class="groupe-formulaire">
-        <label for="mot-de-passe">Mot de passe</label>
-        <input type="password" class="controle-formulaire" id="mot-de-passe" name="mot_passe">
-    </div>
-    <!-- confirmer Mot de passe -->
-    <div class="groupe-formulaire">
-        <label for="confirmation-mot-de-passe">Confirmer le mot de passe</label>
-        <input type="password" class="controle-formulaire" id="confirmation-mot-de-passe" name="confirmation-mot-de-passe">
-    </div>
-    <!-- Prochaine etape -->
-    <button type="submit" class="bouton bouton-primaire" name="action-aller-seconde-etape" value="naviguer">Poursuivre</button>
-    </form>
-</div>
-<?php 
+    <?php
 }
+
 function afficherDeuxiemeEtape($utilisateur, $page = null)
 {
-?>
-<div class="conteneur-inscription">
-    <form method="post">   
-        <!-- nom-->
-        <div class="groupe-formulaire">
-            <label for="nom">Nom</label>
-            <input type="nom" class="controle-formulaire" id="nom" name="nom" value=<?=$utilisateur->getNom();?>>
-        </div>
-        <!-- Prenom -->
-        <div class="groupe-formulaire">
-            <label for="prenom">Prénom</label>
-            <input type="prenom" class="controle-formulaire" id="prenom" name="prenom" value=<?=$utilisateur->getPrenom();?>>
-        </div>
-        <!-- Adresse -->
-        <div class="groupe-formulaire">
-            <label for="adresse">Adresse</label>
-            <input type="adresse" class="controle-formulaire" id="adresse" name="adresse_postal" value=<?=$utilisateur->getAdresse_postal();?>>
-        </div>
-        <!-- code postal -->
-        <div class="groupe-formulaire">
-            <label for="code-postal">Code postal</label>
-            <input type="code-postal" class="controle-formulaire" id="code-postal" name="code_postal" value=<?=$utilisateur->getCode_postal();?>>
-        </div>
-        <!-- vile -->
-        <div class="groupe-formulaire">
-            <label for="ville">Ville</label>
-            <input type="ville" class="controle-formulaire" id="ville" name="ville" value=<?=$utilisateur->getVille();?>>
-        </div>
-        <!-- condition d'utilisation -->
-        <div class="groupe-formulaire bouton-cocher">
-            <label class="etiquette-bouton-cocher">
-            <input class="saisie-bouton-cocher" type="checkbox" name="accepter-condition" value="coche"> J'accepte les <a href="#">conditions d'utilisations</a>
-        </div>    
-        <!-- finaliser ou revenir-->
-        <button type="submit" class="bouton bouton-primaire" name="action-aller-premiere-etape" value="naviguer">Revenir</button>
-        <button type="submit" class="bouton bouton-primaire" name="action-inscrire" value="inscrire">S'inscrire</button>
-    </form>
-</div>
+    ?>
+    <div class="conteneur-inscription">
+        <form method="post">
+            <!-- nom-->
+            <div class="groupe-formulaire">
+                <label for="nom">Nom</label>
+                <input type="nom" class="controle-formulaire" id="nom" name="nom" value=<?= $utilisateur->getNom(); ?>>
+            </div>
+            <!-- Prenom -->
+            <div class="groupe-formulaire">
+                <label for="prenom">Prénom</label>
+                <input type="prenom" class="controle-formulaire" id="prenom" name="prenom"
+                       value=<?= $utilisateur->getPrenom(); ?>>
+            </div>
+            <!-- Adresse -->
+            <div class="groupe-formulaire">
+                <label for="adresse">Adresse</label>
+                <input type="adresse" class="controle-formulaire" id="adresse" name="adresse_postal"
+                       value=<?= $utilisateur->getAdresse_postal(); ?>>
+            </div>
+            <!-- code postal -->
+            <div class="groupe-formulaire">
+                <label for="code-postal">Code postal</label>
+                <input type="code-postal" class="controle-formulaire" id="code-postal" name="code_postal"
+                       value=<?= $utilisateur->getCode_postal(); ?>>
+            </div>
+            <!-- vile -->
+            <div class="groupe-formulaire">
+                <label for="ville">Ville</label>
+                <input type="ville" class="controle-formulaire" id="ville" name="ville"
+                       value=<?= $utilisateur->getVille(); ?>>
+            </div>
+            <!-- condition d'utilisation -->
+            <div class="groupe-formulaire bouton-cocher">
+                <label class="etiquette-bouton-cocher">
+                    <input class="saisie-bouton-cocher" type="checkbox" name="accepter-condition" value="coche">
+                    J'accepte les <a href="#">conditions d'utilisations</a>
+            </div>
+            <!-- finaliser ou revenir-->
+            <button type="submit" class="bouton bouton-primaire" name="action-aller-premiere-etape" value="naviguer">
+                Revenir
+            </button>
+            <button type="submit" class="bouton bouton-primaire" name="action-inscrire" value="inscrire">S'inscrire
+            </button>
+        </form>
+    </div>
 
-<?php
+    <?php
 }
+
 function afficherErreur()
-{?>
-<div id="message-erreur">
-    <strong>Attention!</strong> Un ou plusieurs champs sont incorectes
-</div>    
-<?php
+{
+    ?>
+    <div id="message-erreur">
+        <strong>Attention!</strong> Un ou plusieurs champs sont incorectes
+    </div>
+    <?php
 }
+
 function afficherPage($utilisateur, $page = null)
 {
-    if(!is_object($page)) $page = (object)[];
+    if (!is_object($page)) $page = (object)[];
     afficherEntete($page);
-    if($page->isPremiereEtape ?? false)
-    {
-        if($page->isEnErreur == true)
-        {
+    if ($page->isPremiereEtape ?? false) {
+        if ($page->isEnErreur == true) {
             afficherErreur();
         }
         afficherPremiereEtape($utilisateur, $page);
-    }
-    elseif($page->isSecondeEtape ?? false)
-    {
-        if($page->isEnErreur == true)
-        {
+    } elseif ($page->isSecondeEtape ?? false) {
+        if ($page->isEnErreur == true) {
             afficherErreur();
         }
         afficherDeuxiemeEtape($utilisateur, $page);
     }
     afficherPiedDePage($page);
 }
+
 require_once("../../../microcabroue_publique/action/action-inscription.php");
 ?>
--- src/microcabroue_publique/action/action-inscription.php
+++ src/microcabroue_publique/action/action-inscription.php
@@ -1,50 +1,42 @@
 <?php
 require_once("../../../microcabroue_com_commun/modele/Utilisateur.php");
 
-function ajouterUtilisateur($utilisateur) {
+function ajouterUtilisateur($utilisateur)
+{
 
     //Ajouter la personne avec $personne dans la BD.
     //Vide Pour l'instant
-   
+
 }
 
-$utilisateur = new Utilisateur((object) $_POST);
+$utilisateur = new Utilisateur((object)$_POST);
 
-if(isset($_POST["action-aller-premiere-etape"]) && $_POST["action-aller-premiere-etape"] == "naviguer")
-{
+if (isset($_POST["action-aller-premiere-etape"]) && $_POST["action-aller-premiere-etape"] == "naviguer") {
     $page->isPremiereEtape = true;
     $page->isSecondeEtape = false;
     $page->isEnErreur = false;
 }
-if(isset($_POST["action-aller-seconde-etape"]) && $_POST["action-aller-seconde-etape"] == "naviguer")
-{
-    if($_POST["confirmation-mot-de-passe"] == $_POST["mot_passe"] && $_POST["mot_passe"] != "" && $_POST["pseudo"] != "" && $_POST["mail"] != "") 
-    {
+if (isset($_POST["action-aller-seconde-etape"]) && $_POST["action-aller-seconde-etape"] == "naviguer") {
+    if ($_POST["confirmation-mot-de-passe"] == $_POST["mot_passe"] && $_POST["mot_passe"] != "" && $_POST["pseudo"] != "" && $_POST["mail"] != "") {
         $page->isPremiereEtape = false;
         $page->isSecondeEtape = true;
         $page->isEnErreur = false;
-    }
-    else
-    {
+    } else {
         $page->isEnErreur = true;
     }
 }
-if(isset($_POST["action-inscrire"]) && $_POST["action-inscrire"] == "inscrire")
-{
+if (isset($_POST["action-inscrire"]) && $_POST["action-inscrire"] == "inscrire") {
     //echo "<script>alert(\"".."\")</script>"; 
-    if($_POST["nom"] != "" && $_POST["prenom"] != "" && $_POST["adresse_postal"] != "" && $_POST["code_postal"] != "" && $_POST["ville"] != "" && $_POST['accepter-condition'] == "coche")
-    {
+    if ($_POST["nom"] != "" && $_POST["prenom"] != "" && $_POST["adresse_postal"] != "" && $_POST["code_postal"] != "" && $_POST["ville"] != "" && $_POST['accepter-condition'] == "coche") {
         $page->isEnErreur = false;
         ajouterPersonne($utilisateur);
         //echo "<script>alert(\"Inscription! WOOOOOOOOOOO\")</script>"; 
-    }
-    else
-    {
-        echo "<script>alert(\"rentre dans erreur de la deuxieme etape\")</script>"; 
+    } else {
+        echo "<script>alert(\"rentre dans erreur de la deuxieme etape\")</script>";
         $page->isPremiereEtape = false;
         $page->isSecondeEtape = true;
         $page->isEnErreur = true;
     }
 }
-afficherPage($utilisateur ,$page);
+afficherPage($utilisateur, $page);
 ?>
