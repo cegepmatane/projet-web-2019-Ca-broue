65630528dbd443ba59fa1e54e382de8b1a349416
Mezeeh <1643216@cgmatane.qc.ca>
2019-02-07 14:24:11 -0500

    feat : fragment formulaire pour template goodie/utilisateur/biere #17

---
+++ src/microcabroue/administration/vue/fragment/formulaire-fragment.php
@@ -0,0 +1,21 @@
+<?php
+
+    function afficherFormulaire($page = null){
+        if(!is_object($page))
+            return;
+
+        switch($page->type){
+            case "biere":
+                include "fragment/formulaire-biere-fragment.php";
+                break;
+            case "goodie":
+                include "fragment/formulaire-goodie-fragment.php";
+                break;
+            case "utilisateur":
+                include "fragment/formulaire-utilisateur-fragment.php";
+                break;
+        }
+
+        afficherChamps($page);
+    }
+?>
