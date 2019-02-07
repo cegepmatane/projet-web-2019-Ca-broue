27cb200bc0f96b6249823a1aaf8a77daa15cfff1
Mezeeh <1643216@cgmatane.qc.ca>
2019-02-07 14:27:03 -0500

    feat : fragment formulaire biere #17

---
+++ src/microcabroue/administration/vue/fragment/formulaire-biere-fragment.php
@@ -0,0 +1,30 @@
+<?php
+    function afficherChamps($page = null){
+        if(!is_object($page))
+            return;
+
+        if(isset($page->biere))
+            $biere = $page->biere;
+
+        ?>
+        <form action="<?= $page->urlRetour; ?>" method="post">
+            <div>
+                <label for="marque">Marque de la bière</label>
+                <input value="<?= isset($biere) ? $biere->marque : null ?>" name='marque' id='marque-ajout-biere' type='text'>
+            </div>
+
+            <div>
+                <label for="description">Description de la bière</label>
+                <textarea name='description' id='description-ajout-biere' cols='30' rows='10'><?= isset($biere) ? $biere->description : null ; ?></textarea>
+            </div>
+            
+            <div>
+                <label for="pourcentage">Pourcentage d'alcool de la bière</label>
+                <input value="<?= isset($biere) ? $biere->pourcentageAlcool : null ; ?>" type="text" name="pourcentage" id="pourcentage-alcool-ajout-biere">
+            </div>
+
+            <input type="submit" value=<?= $page->action == "ajouter" ? "Ajouter" : "Enregistrer"?>>
+        </form>
+        <?php
+    }
+?>
