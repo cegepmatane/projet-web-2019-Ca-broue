cd8c2880bb411491b43e259b66282cafef2f61bb
Mezeeh <1643216@cgmatane.qc.ca>
2019-02-07 00:00:02 -0500

    feat : fragment pour les listes d'items #17

---
+++ src/microcabroue/administration/vue/fragment/liste-fragment.php
@@ -0,0 +1,27 @@
+<?php
+    function afficherListe($page = null){
+        if(!is_object($page))
+            return;
+
+        ?>
+
+            <h2><?= $page->titre; ?></h2>
+
+            <?php if($page->type != "utilisateur"){?>
+                <a href="./vue-ajouter-<?= $page->type; ?>.php">Ajouter</a>
+            <?php } ?>
+        <?php
+
+        foreach($page->liste as $item){
+            ?>
+
+            <div>
+                <?= $item->nom; ?>
+                <a href="vue-modifier-<?= $page->type; ?>.php?id=<?= $item->id; ?>">Modifier</a>
+                <a href="#supprimer?id=<?= $item->id; ?>">Supprimer</a>
+            </div>
+
+            <?php
+        }
+    }
+?>
