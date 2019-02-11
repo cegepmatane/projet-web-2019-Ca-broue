cf628ad18de9743d0bd641030c065cf7b7a5850d
Unknown <beauquel.alexandre@gmail.com>
2019-02-11 17:33:31 -0500

    wip : charger les categories de goodies de la base de données

--- src/microcabroue_publique/accesseur/AccesseurEntiteCategorieGoodie.php
+++ src/microcabroue_publique/accesseur/AccesseurEntiteCategorieGoodie.php
@@ -9,23 +9,25 @@
 class AccesseurEntiteCategorieGoodie
 {
 
+    const SELECT_TOUTES_LES_CATEGORIES = "select * from ". CategorieGoodie::TABLE;
+
     public function recupererListeEntiteCategorieGoodie(){
+        $bdd = new PDO('mysql:host=localhost;dbname=cabroue;charset=utf8', 'root', ''); //TODO pour tester le base de donnees et la requete
+
         $listeCategorie=[];
-        $listeCategorie[]= new CategorieGoodie((object)
-        [
-            "libelle" =>"Poster",
-            "id" => 1
-        ]);
-        $listeCategorie[]= new CategorieGoodie((object)
-        [
-            "libelle" =>"Buck",
-            "id" => 2
-        ]);
-        $listeCategorie[]= new CategorieGoodie((object)
-        [
-            "libelle" =>"Vêtements",
-            "id" => 3
-        ]);
+        $curseur =$bdd->prepare(self::SELECT_TOUTES_LES_CATEGORIES);
+        $curseur->execute();
+        $donnees = $curseur->fetchAll(PDO::FETCH_ASSOC);
+        if (count($donnees) > 0) {
+            foreach ($donnees as $maLigne) {
+                $categorie = new CategorieGoodie((object)
+                [
+                    CategorieGoodie::ID => $maLigne[CategorieGoodie::ID],
+                    CategorieGoodie::LIBELLE_FR =>$maLigne[CategorieGoodie::LIBELLE_FR]
+                ]);
+                $listeCategorie[] = $categorie;
+            }
+        }
         return $listeCategorie;
     }
 }
