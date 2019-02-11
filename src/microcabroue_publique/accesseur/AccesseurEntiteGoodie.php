4da5f4aa3a9b39fa12d7d0ed21d9da6a73f6da16
Unknown <beauquel.alexandre@gmail.com>
2019-02-11 17:52:23 -0500

    wip : chargez les goodies d'une categorie

--- src/microcabroue_publique/accesseur/AccesseurEntiteGoodie.php
+++ src/microcabroue_publique/accesseur/AccesseurEntiteGoodie.php
@@ -8,6 +8,8 @@
 
 class AccesseurEntiteGoodie
 {
+    const SELECT_GOODIES_PAR_CATEGORIE = "select * from ". CategorieGoodie::TABLE ." WHERE ".Goodie::ID_CATEGORIE."=:id_categorie";
+
     public function recupererListeEntiteGoodie(){
 
         $listeGoodies = [
@@ -34,5 +36,30 @@ class AccesseurEntiteGoodie
         ];
 
         return $listeGoodies;
+    }
+
+    public function recupererListeEntiteGoodieParCategorie(int $id_categorie){
+        $bdd = new PDO('mysql:host=localhost;dbname=cabroue;charset=utf8', 'root', ''); //TODO pour tester le base de donnees et la requete
+        $listeGoodie=[];
+        $curseur =$bdd->prepare(self::SELECT_GOODIES_PAR_CATEGORIE);
+        $curseur->bindParam(":id_categorie",$id_categorie);
+        $curseur->execute();
+        $donnees = $curseur->fetchAll(PDO::FETCH_ASSOC);
+        if (count($donnees) > 0) {
+            foreach ($donnees as $maLigne) {
+                $goodie = new Goodie((object)
+                [
+                    Goodie::ID => $maLigne[Goodie::ID],
+                    Goodie::NOM_FR =>$maLigne[Goodie::NOM_FR],
+                    Goodie::NOM_EN =>$maLigne[Goodie::NOM_EN],
+                    Goodie::DESCRIPTION_FR =>$maLigne[Goodie::DESCRIPTION_FR],
+                    Goodie::DESCRIPTION_EN =>$maLigne[Goodie::DESCRIPTION_EN],
+                    Goodie::DESCRIPTION_LONGUE_EN =>$maLigne[Goodie::DESCRIPTION_LONGUE_EN],
+                    Goodie::DESCRIPTION_LONGUE_FR =>$maLigne[Goodie::DESCRIPTION_LONGUE_FR],
+                ]);
+                $listeGoodie[] = $goodie;
+            }
+        }
+        return $listeGoodie;
     }
 }
