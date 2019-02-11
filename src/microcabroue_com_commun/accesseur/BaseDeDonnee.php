9875047808486cbc1ace5468ef29cb219a33ac75
Florian <flo>
2019-02-11 18:21:12 -0500

    wip : redirection vers les bons chemin dans configuration.php (NE PAS COMMIT CE FICHIER LA )

---
+++ src/microcabroue_com_commun/accesseur/BaseDeDonnee.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once("../../../microcabroue_com_commun/configuration/configuration.php");
+
+class BaseDeDonnee {
+  private static $pdo = null;
+
+  public static function getConnexion() {
+    if (!self::$pdo) {
+        $host = BASE_DE_DONNEE_HOST;
+        $db   = BASE_DE_DONNEE_NOM;
+        $user = BASE_DE_DONNEE_UTILISATEUR;
+        $pass = BASE_DE_DONNEE_MOT_DE_PASSE;
+        $charset = BASE_DE_DONNEE_CHARSET;
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        try {
+             $pdo = new PDO($dsn, $user, $pass, $options);
+        } catch (\PDOException $e) {
+             throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }      self::$isInitialise = true;
+    }
+
+    return self::$pdo;
+  }
+}
+
+// EOF
