2aa3fc6d43dd54373e1867b3ee445b31eae0e98c
Sedrick <sedrickidd@hotmail.com>
2019-02-21 13:59:22 -0500

    wip : ajout css dans le fragment ajout de bierre #17

--- src/microcabroue/administration/vue/fragment/formulaire-biere-fragment.php
+++ src/microcabroue/administration/vue/fragment/formulaire-biere-fragment.php
@@ -8,22 +8,22 @@
 
         ?>
         <form action="<?= $page->urlRetour; ?>" method="post">
-            <div>
+            <div class="groupe-formulaire">
                 <label for="marque">Marque de la bière</label>
-                <input value="<?= isset($biere) ? $biere->marque : null ?>" name='marque' id='marque-ajout-biere' type='text'>
+                <input value="<?= isset($biere) ? $biere->marque : null ?>" name='marque' id='marque-ajout-biere' type='text' class="controle-formulaire">
             </div>
 
-            <div>
+            <div class="groupe-formulaire">
                 <label for="description">Description de la bière</label>
-                <textarea name='description' id='description-ajout-biere' cols='30' rows='10'><?= isset($biere) ? $biere->description : null ; ?></textarea>
+                <textarea name='description' id='description-ajout-biere' cols='30' rows='10' class="controle-formulaire"><?= isset($biere) ? $biere->description : null ; ?></textarea>
             </div>
             
-            <div>
+            <div class="groupe-formulaire">
                 <label for="pourcentage">Pourcentage d'alcool de la bière</label>
-                <input value="<?= isset($biere) ? $biere->pourcentageAlcool : null ; ?>" type="text" name="pourcentage" id="pourcentage-alcool-ajout-biere">
+                <input value="<?= isset($biere) ? $biere->pourcentageAlcool : null ; ?>" type="text" name="pourcentage" id="pourcentage-alcool-ajout-biere" class="controle-formulaire">
             </div>
 
-            <input type="submit" value=<?= $page->action == "ajouter" ? "Ajouter" : "Enregistrer"?>>
+            <input class="bouton bouton-vert" type="submit" value=<?= $page->action == "ajouter" ? "Ajouter" : "Enregistrer"?>>
         </form>
         <?php
     }
