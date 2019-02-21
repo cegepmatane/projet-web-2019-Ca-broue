8978f3e2b1c0e1d463dac0cd0edc71b5b5befacf
Michael Turcotte <mezeeh@hotmail.com>
2019-02-21 16:55:54 -0500

    refactor : correction des champs form goodie pour representer la table #17

--- src/microcabroue/administration/vue/fragment/formulaire-goodie-fragment.php
+++ src/microcabroue/administration/vue/fragment/formulaire-goodie-fragment.php
@@ -1,4 +1,7 @@
 <?php
+    require_once (CHEMIN_CODE."microcabroue_com_commun/modele/Goodie.php");
+    require_once (CHEMIN_CODE."microcabroue_com_commun/modele/CategorieGoodie.php");
+
     function afficherChamps($page = null){
         if(!is_object($page))
             return;
@@ -9,18 +12,50 @@
         ?>
         <form action="<?= $page->urlRetour; ?>" method="post">
             <div>
-                <label for="nom">Nom du goodie</label>
-                <input value="<?= isset($goodie) ? $goodie->nom : null ?>" name='nom' id='nom-modification-goodie' type='text'>
+                <label for=<?= Goodie::NOM_FR; ?>>Nom du goodie (fr)</label>
+                <input value="<?= isset($goodie) ? $goodie->getNomFr() : null ?>" name=<?= Goodie::NOM_FR; ?> id='nom-modification-goodie-fr' type='text'>
             </div>
 
             <div>
-                <label for="description">Description du goodie</label>
-                <textarea name='description' id='description-modification-goodie' cols='30' rows='10'><?= isset($goodie) ? $goodie->description : null ?></textarea>
+                <label for=<?= Goodie::NOM_EN; ?>>Nom du goodie (en)</label>
+                <input value="<?= isset($goodie) ? $goodie->getNomEn() : null ?>" name=<?= Goodie::NOM_EN; ?> id='nom-modification-goodie-en' type='text'>
+            </div>
+
+            <div>
+                <label for=<?= Goodie::ID_CATEGORIE; ?>>Catégorie</label>
+                <select name=<?= Goodie::ID_CATEGORIE; ?> id="categorie-modification-goodie">
+                    <?php if($page->action == "ajout"){ ?>
+                        <option value="" selected disabled hidden>Choisir une catégorie</option>
+                    <?php } ?>    
+                    <?php foreach ($page->listeCategories as $categorie){ ?>
+                        <option <?= isset($goodie) ? ($goodie->getIdCategorie() == $categorie->getId()) ? "selected" : null : null; ?> value=<?= $categorie->getId() ?>><?= $categorie->getLibelleFr(); ?></option>
+                    <?php } ?>
+                </select>
+            </div>
+
+            <div>
+                <label for=<?= Goodie::DESCRIPTION_LONGUE_FR; ?>>Description longue du goodie (fr)</label>
+                <textarea name=<?= Goodie::DESCRIPTION_LONGUE_FR; ?> id='description-longue-modification-goodie-fr' cols='30' rows='5'><?= isset($goodie) ? $goodie->getDescriptionLongueFr() : null ?></textarea>
+            </div>
+
+            <div>
+                <label for=<?= Goodie::DESCRIPTION_LONGUE_EN; ?>>Description longue du goodie (en)</label>
+                <textarea name=<?= Goodie::DESCRIPTION_LONGUE_FR; ?> id='description-longue-modification-goodie-en' cols='30' rows='5'><?= isset($goodie) ? $goodie->getDescriptionLongueEn() : null ?></textarea>
+            </div>
+
+            <div>
+                <label for=<?= Goodie::DESCRIPTION_FR; ?>>Description courte du goodie (fr)</label>
+                <textarea <?= Goodie::DESCRIPTION_FR; ?> id='description-courte-modification-goodie-fr' cols='30' rows='5'><?= isset($goodie) ? $goodie->getDescriptionFr() : null ?></textarea>
+            </div>
+
+            <div>
+                <label for=<?= Goodie::DESCRIPTION_EN; ?>>Description courte du goodie (en)</label>
+                <textarea name=<?= Goodie::DESCRIPTION_EN; ?> id='description-courte-modification-goodie-en' cols='30' rows='5'><?= isset($goodie) ? $goodie->getDescriptionEn() : null ?></textarea>
             </div>
             
             <div>
-                <label for="prix">Prix du goodie</label>
-                <input value="<?= isset($goodie) ? $goodie->prix : null ?>" type="text" name="prix" id="prix-modification-goodie">
+                <label for=<?= Goodie::PRIX; ?>>Prix du goodie</label>
+                <input value="<?= isset($goodie) ? $goodie->getPrix() : null ?>" type="text" name=<?= Goodie::PRIX; ?> id="prix-modification-goodie">
             </div>
 
             <input type="submit" value=<?= $page->action == "ajouter" ? "Ajouter" : "Enregistrer"?>>
