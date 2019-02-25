e1d7e560f8ca75e80bce854fab49c483327f81b8
Sedrick <sedrickidd@hotmail.com>
2019-02-24 20:06:05 -0500

    wip : Ajout du css semantique formulaire goodies #17

--- src/microcabroue/administration/vue/fragment/formulaire-goodie-fragment.php
+++ src/microcabroue/administration/vue/fragment/formulaire-goodie-fragment.php
@@ -11,19 +11,19 @@
 
         ?>
         <form action="<?= $page->urlRetour; ?>" method="post">
-            <div>
+            <div class="groupe-formulaire">
                 <label for=<?= Goodie::NOM_FR; ?>>Nom du goodie (fr)</label>
-                <input value="<?= isset($goodie) ? $goodie->getNomFr() : null ?>" name=<?= Goodie::NOM_FR; ?> id='nom-modification-goodie-fr' type='text'>
+                <input class="controle-formulaire" value="<?= isset($goodie) ? $goodie->getNomFr() : null ?>" name=<?= Goodie::NOM_FR; ?> id='nom-modification-goodie-fr' type='text'>
             </div>
 
-            <div>
+            <div class="groupe-formulaire"> 
                 <label for=<?= Goodie::NOM_EN; ?>>Nom du goodie (en)</label>
-                <input value="<?= isset($goodie) ? $goodie->getNomEn() : null ?>" name=<?= Goodie::NOM_EN; ?> id='nom-modification-goodie-en' type='text'>
+                <input class="controle-formulaire" value="<?= isset($goodie) ? $goodie->getNomEn() : null ?>" name=<?= Goodie::NOM_EN; ?> id='nom-modification-goodie-en' type='text'>
             </div>
 
-            <div>
+            <div class="groupe-formulaire">
                 <label for=<?= Goodie::ID_CATEGORIE; ?>>Catégorie</label>
-                <select name=<?= Goodie::ID_CATEGORIE; ?> id="categorie-modification-goodie">
+                <select class="controle-formulaire" name=<?= Goodie::ID_CATEGORIE; ?> id="categorie-modification-goodie">
                     <?php if($page->action == "ajout"){ ?>
                         <option value="" selected disabled hidden>Choisir une catégorie</option>
                     <?php } ?>    
@@ -33,32 +33,32 @@
                 </select>
             </div>
 
-            <div>
+            <div class="groupe-formulaire">
                 <label for=<?= Goodie::DESCRIPTION_LONGUE_FR; ?>>Description longue du goodie (fr)</label>
-                <textarea name=<?= Goodie::DESCRIPTION_LONGUE_FR; ?> id='description-longue-modification-goodie-fr' cols='30' rows='5'><?= isset($goodie) ? $goodie->getDescriptionLongueFr() : null ?></textarea>
+                <textarea class="controle-formulaire" name=<?= Goodie::DESCRIPTION_LONGUE_FR; ?> id='description-longue-modification-goodie-fr' cols='30' rows='5'><?= isset($goodie) ? $goodie->getDescriptionLongueFr() : null ?></textarea>
             </div>
 
-            <div>
+            <div class="groupe-formulaire">
                 <label for=<?= Goodie::DESCRIPTION_LONGUE_EN; ?>>Description longue du goodie (en)</label>
-                <textarea name=<?= Goodie::DESCRIPTION_LONGUE_FR; ?> id='description-longue-modification-goodie-en' cols='30' rows='5'><?= isset($goodie) ? $goodie->getDescriptionLongueEn() : null ?></textarea>
+                <textarea class="controle-formulaire" name=<?= Goodie::DESCRIPTION_LONGUE_FR; ?> id='description-longue-modification-goodie-en' cols='30' rows='5'><?= isset($goodie) ? $goodie->getDescriptionLongueEn() : null ?></textarea>
             </div>
 
-            <div>
+            <div class="groupe-formulaire">
                 <label for=<?= Goodie::DESCRIPTION_FR; ?>>Description courte du goodie (fr)</label>
-                <textarea <?= Goodie::DESCRIPTION_FR; ?> id='description-courte-modification-goodie-fr' cols='30' rows='5'><?= isset($goodie) ? $goodie->getDescriptionFr() : null ?></textarea>
+                <textarea class="controle-formulaire" <?= Goodie::DESCRIPTION_FR; ?> id='description-courte-modification-goodie-fr' cols='30' rows='5'><?= isset($goodie) ? $goodie->getDescriptionFr() : null ?></textarea>
             </div>
 
-            <div>
+            <div class="groupe-formulaire">
                 <label for=<?= Goodie::DESCRIPTION_EN; ?>>Description courte du goodie (en)</label>
-                <textarea name=<?= Goodie::DESCRIPTION_EN; ?> id='description-courte-modification-goodie-en' cols='30' rows='5'><?= isset($goodie) ? $goodie->getDescriptionEn() : null ?></textarea>
+                <textarea class="controle-formulaire" name=<?= Goodie::DESCRIPTION_EN; ?> id='description-courte-modification-goodie-en' cols='30' rows='5'><?= isset($goodie) ? $goodie->getDescriptionEn() : null ?></textarea>
             </div>
             
-            <div>
+            <div class="groupe-formulaire">
                 <label for=<?= Goodie::PRIX; ?>>Prix du goodie</label>
-                <input value="<?= isset($goodie) ? $goodie->getPrix() : null ?>" type="text" name=<?= Goodie::PRIX; ?> id="prix-modification-goodie">
+                <input class="controle-formulaire" value="<?= isset($goodie) ? $goodie->getPrix() : null ?>" type="text" name=<?= Goodie::PRIX; ?> id="prix-modification-goodie">
             </div>
 
-            <input type="submit" value=<?= $page->action == "ajouter" ? "Ajouter" : "Enregistrer"?>>
+            <input class="bouton bouton-vert" type="submit" value=<?= $page->action == "ajouter" ? "Ajouter" : "Enregistrer"?>>
         </form>
         <?php
     }
