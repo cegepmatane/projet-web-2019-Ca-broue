75b0aa2b04cdff29e41ea4980ba0bae21a4a0668
Sedrick <sedrickidd@hotmail.com>
2019-02-26 13:22:31 -0500

    wip : ajout du css de la liste utilisateur adminstration #17

--- src/microcabroue/administration/vue/fragment/liste-utilisateur-fragment.php
+++ src/microcabroue/administration/vue/fragment/liste-utilisateur-fragment.php
@@ -6,13 +6,13 @@
         if(isset($page->listeUtilisateurs)){
             foreach($page->listeUtilisateurs as $utilisateur){
                 ?>
-
                 <div>
-                    <?= $utilisateur->nom; ?>
-                    <a href="vue-modifier-<?= $page->type; ?>.php?id=<?= $utilisateur->id;?>" class="bouton bouton-bleu">Modifier</a>
-                    <a href="#supprimer?id=<?= $utilisateur->id; ?>" class="bouton bouton-rouge">Supprimer</a>
+                 <?= $utilisateur->nom; ?>
+                <div class="boutons-liste"> 
+                    <a href="vue-modifier-<?= $page->type; ?>.php?id=<?= $utilisateur->id;?>" class="bouton bouton-bleu bouton-liste">Modifier</a>
+                    <a href="#supprimer?id=<?= $utilisateur->id; ?>" class="bouton bouton-rouge bouton-liste">Supprimer</a>
                 </div>
-
+                </div>
                 <?php
             }
         }
