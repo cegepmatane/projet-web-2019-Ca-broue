5b6950acc9d4f1fa30834997b0fa224fc25ad8dd
abeauquel <alex2425b@gmail.com>
2019-02-28 11:22:27 -0500

    fix : erreurs sur les noms des getteurs #24

--- src/microcabroue/administration/vue/fragment/formulaire-categorie-fragment.php
+++ src/microcabroue/administration/vue/fragment/formulaire-categorie-fragment.php
@@ -11,27 +11,34 @@ function afficherChamps($page = null){
     if(!is_object($page))
         return;
 
-    if(isset($page->categorie))
+    if(!isset($page->categorie) || $page->categorie == null) {
+        echo "<div class=\"alert alert-danger\" role=\"alert\">
+        Erreur sur le chargement de la catégorie
+        </div>";
+        return;
+    }
+
+
         /** @var CategorieGoodie $categorie */
         $categorie = $page->categorie;
-
     ?>
     <form method="post">
         <input type="hidden" name="id" value=<?= isset($categorie) ? $categorie->getId() : null ?>>
 
         <div class="groupe-formulaire">
             <label for=<?= CategorieGoodie::LIBELLE_FR; ?>>Libelle de la categorie (fr)</label>
-            <input class="controle-formulaire" value="<?= isset($categorie) ? $categorie->get() : null ?>" name=<?= CategorieGoodie::LIBELLE_FR; ?> id='libelle-modification-categorie-fr' type='text'>
+            <input class="controle-formulaire" value="<?= isset($categorie) ? $categorie->getLibelleFr() : null ?>" name=<?= CategorieGoodie::LIBELLE_FR; ?> id='libelle-modification-categorie-fr' type='text'>
         </div>
 
         <div class="groupe-formulaire">
             <label for=<?= CategorieGoodie::LIBELLE_EN; ?>>Libelle de la categorie  (en)</label>
-            <input class="controle-formulaire" value="<?= isset($categorie) ? $categorie->getNomEn() : null ?>" name=<?= CategorieGoodie::LIBELLE_EN; ?> id='libelle-modification-categorie-en' type='text'>
+            <input class="controle-formulaire" value="<?= isset($categorie) ? $categorie->getLibelleEn() : null ?>" name=<?= CategorieGoodie::LIBELLE_EN; ?> id='libelle-modification-categorie-en' type='text'>
         </div>
 
         <button class="bouton bouton-vert" name="action-modifier" type="submit" value="<?= $page->action; ?>"><?= $page->action == "ajout" ? "Ajouter" : "Enregistrer"?></button>
     </form>
     <?php
+
 }
 
 require_once (CHEMIN_CODE."microcabroue_com_administration/action/action-modifier-categorie.php");
