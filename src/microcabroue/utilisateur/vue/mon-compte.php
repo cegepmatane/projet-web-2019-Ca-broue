f9cfd3428263f0658778bf403838e76ed14dd979
Sedrick <sedrickidd@hotmail.com>
2019-03-07 14:17:26 -0500

    wip : page html mon-compte affichable #25

--- src/microcabroue/utilisateur/vue/mon-compte.php
+++ src/microcabroue/utilisateur/vue/mon-compte.php
@@ -5,17 +5,36 @@ require_once(CHEMIN_SRC_DEV . "microcabroue/commun/vue/fragment/pied-de-page-fra
 
 $page = (Object)
 [
-   // "style" => "utilisateur/decoration/inscription.css",
+   // "style" => "utilisateur/decoration/mon-compte.css",
     "titre" => "Mon compte"
 ];
 
 
-function afficherPage($page = null)
+function afficherPage($page = null, $utilisateur = null)
 {
     if (!is_object($page)) $page = (object)[];
     afficherEntete($page);
     ?>
-    
+        <div class='container'>
+            <h2>Bonjour <?=$utilisateur->getPrenom()?></h2>
+            <div class='container' id="information-compte">
+                <h4>Informations sur le compte</h4>
+                <p>Nom d'utilisateur: <?=$utilisateur->getPseudo()?>
+                <p>Mot de Passe: <?=$utilisateur->getPseudo()?>
+                <p>Adresse email: <?$utilisateur->getMail()?>
+                <a class='button'>Modifier</a>
+            </div>
+            <div class="container" id="information-personnelle">
+                <h4>Informations personnelles</h4>
+                <p>Nom: <?=$utilisateur->getNom()?></p>
+                <p>Prenom: <?=$utilisateur->getPrenom()?></p>
+                <p>Adresse: <?=$utilisateur->getAdresse_postal()?></p>
+                <p>Code postal: <?=$utilisateur->getCode_postal()?></p>
+                <p>Ville: <?=$utilisateur->getVille()?></p> 
+                <a class='button'>Modifier</a>
+            </div>    
+
+        </div>
     <?php
     afficherPiedDePage($page);
 
