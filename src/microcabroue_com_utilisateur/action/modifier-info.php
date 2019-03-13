66fc367d2ea4e2751d333fc9f5fc0d15ffaf146d
Sedrick <sedrickidd@hotmail.com>
2019-03-13 14:50:15 -0400

    wip : afichage de la bonne page pour la modification de compte #25

--- src/microcabroue_com_utilisateur/action/modifier-info.php
+++ src/microcabroue_com_utilisateur/action/modifier-info.php
@@ -1,18 +1,21 @@
 <?php
 require_once(CHEMIN_SRC_DEV . "microcabroue_com_commun/accesseur/AccesseurUtilisateur.php");
-
+session_start();
 //echo "<script>alert('my nama jeff')</script>";
-print_r($_POST);
-
-if(isset($_POST['modifier']) && $_POST['modifier'] == "modifier-compte")
+print_r($_SESSION);
+if(isset($_SESSION['page-modifier']) && $_SESSION['page-modifier'] == "modifier-compte")
 {
+    session_abort();
     $page->titre = "Modifier vos informations du compte";
+    afficherEntete($page);
     afficherPageCompte($page);
 }
 
-if (isset($_POST['modifier']) && $_POST['modifier'] == "modifier-info")
+if (isset($_SESSION['page-modifier']) && $_SESSION['page-modifier'] == "modifier-info")
 {
+    session_abort();
     $page->titre = "Modifier vos informations personnelles";
+    afficherEntete($page);
     afficherPageInfo($page);
 }
 
--- src/microcabroue_com_utilisateur/action/mon-compte.php
+++ src/microcabroue_com_utilisateur/action/mon-compte.php
@@ -1,14 +1,17 @@
 <?php
 //session_start(); 
 require_once(CHEMIN_SRC_DEV . "microcabroue_com_commun/accesseur/AccesseurUtilisateur.php");
+afficherEntete($page);
 
+//print_r($_SESSION);
 
 if((isset($_POST['modifier']) && $_POST['modifier'] == "modifier-compte") || (isset($_POST['modifier']) && $_POST['modifier'] == "modifier-info"))
 {
+    $_SESSION['page-modifier'] = $_POST['modifier'];
     header('location: modifier-compte');
 }
 
-afficherEntete($page);
+
 $accesseur = new AccesseurUtilisateur();
 $id = $_SESSION['id'];
 $utilisateur = $accesseur->recevoirUtilisateur($id);
