e1e098094aef93a7051142c7280f2aa76be294ab
Florian <flolevy33@gmail.com>
2019-03-14 10:36:05 -0400

    feat: suppression start_session et abort

--- src/microcabroue_com_utilisateur/action/modifier-info.php
+++ src/microcabroue_com_utilisateur/action/modifier-info.php
@@ -1,6 +1,5 @@
 <?php
 require_once(CHEMIN_SRC_DEV . "microcabroue_com_commun/accesseur/AccesseurUtilisateur.php");
-session_start();
 $accesseur = new AccesseurUtilisateur();
 $id = $_SESSION['id'];
 $utilisateur = $accesseur->recevoirUtilisateur($id);
@@ -21,7 +20,6 @@ if(isset($_POST['enregistrer']) && $_POST['enregistrer'] == "true")
 
 if(isset($_SESSION['page-modifier']) && $_SESSION['page-modifier'] == "modifier-compte")
 {
-    session_abort();
     $page->titre = "Modifier vos informations du compte";
     afficherEntete($page);
     afficherPageCompte($page, $utilisateur);
@@ -29,7 +27,6 @@ if(isset($_SESSION['page-modifier']) && $_SESSION['page-modifier'] == "modifier-
 
 if (isset($_SESSION['page-modifier']) && $_SESSION['page-modifier'] == "modifier-info")
 {
-    session_abort();
     $page->titre = "Modifier vos informations personnelles";
     afficherEntete($page);
     afficherPageInfo($page, $utilisateur);
