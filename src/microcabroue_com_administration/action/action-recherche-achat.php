fba86e1990b2286cafb30a511537f02a292db16b
Sedrick <sedrickidd@hotmail.com>
2019-03-16 17:13:31 -0400

    wip : switch vide pour recherche et verification #30

--- src/microcabroue_com_administration/action/action-recherche-achat.php
+++ src/microcabroue_com_administration/action/action-recherche-achat.php
@@ -1,6 +1,19 @@
 <?php
+
+if(isset($_POST['bouton-rechercher']) && $_POST['bouton-rechercher'] == "rechercher")
+{
+    switch($_POST['choix-recherche'])
+    {
+        case "numero-achat":
+            break;
+        case "date":
+            break;
+        case "produit":
+            break;    
+        case "utilisateur":
+            break;
+    }
+}
+
 afficherPage($page);
-
-
-
 ?>
