0963b5e7e505cbdbf73ba3e2df65fd177ecb2e1f
Sedrick <sedrickidd@hotmail.com>
2019-03-16 21:49:29 -0400

    wip : Recherche par numero fonctionnelle #30

--- src/microcabroue/administration/vue/recherche-achat.php
+++ src/microcabroue/administration/vue/recherche-achat.php
@@ -10,25 +10,11 @@ require_once "fragment/pied-de-page-fragment.php";
      
  ];
 
- function afficherPage($page = null)
+ function afficherResultat($page = null)
  {
     if(!is_object($page))
             $page = (object)[];
-
-    afficherEntete($page);
     ?>
-    <div class="recherche">
-    <form method="post">
-    <select name="choix-recherche">
-    <option value="numero-achat">Numéro d'achat</option>
-    <option value="date">Date</option>
-    <option value="produit">Produit</option>
-    <option value="utilisateur">Client</option>
-    </select>
-    <input type="text">
-    <button type="submit" class="bouton bouton-vert" value="rechercher" name="bouton-rechercher">Rechercher</button>
-    </form>
-    </div>
     <h4>Résultats de recherche</h4>
     <div class="tableau-resultat">
     <table>
@@ -59,10 +45,70 @@ require_once "fragment/pied-de-page-fragment.php";
     </table>
     </div>
 
-
     <?php
     afficherPiedDePage($page);
  }
+    function afficherChoixDeRecherche($page)
+    {
+        if(!is_object($page))
+            $page = (object)[];
+        ?>
+        <div class="conteneur">
+    <form method="post">
+    <select name="choix-recherche">
+    <option value="numero-achat">Numéro d'achat</option>
+    <option value="date">Date</option>
+    <option value="produit">Produit</option>
+    <option value="utilisateur">Client</option>
+    </select>
+    <button type="submit" class="bouton bouton-vert" value="selectionner" name="bouton-choix">Selectionner</button>
+    </form>
+    </div>
+        <?php
+    }
+
+    function afficherParNom($page)
+    {
+        if(!is_object($page))
+            $page = (object)[];
+        ?>
+
+        <?php
+    }
+
+    function afficherParProduit($page)
+    {
+        if(!is_object($page))
+            $page = (object)[];
+        ?>
+        
+        <?php
+    }
+    function afficherParDate($page)
+    {
+        if(!is_object($page))
+            $page = (object)[];
+        ?>
+        
+        <?php
+    }
+    function afficherParNumero($page)
+    {
+        if(!is_object($page))
+            $page = (object)[];
+        ?>
+        <div class="conteneur">
+        <form method="post">
+        <input type="hidden" name="choix-recherche" value="<?=$_POST['choix-recherche']?>">
+        <label for="numero-achat">Numéro de d'achat</label>
+        <input type="text" name="numero-achat" value="000000">
+        <button type="submit" class="bouton bouton-vert" name="bouton-recherche" value="rechercher"> Rechercher</button>
+        </form>
+        </div>
+        <?php
+    }
+
+
  require_once (CHEMIN_CODE."microcabroue_com_administration/action/action-recherche-achat.php");
 
 ?>
--- src/microcabroue_com_administration/action/action-recherche-achat.php
+++ src/microcabroue_com_administration/action/action-recherche-achat.php
@@ -1,10 +1,55 @@
 <?php
+require_once (CHEMIN_SRC_DEV."microcabroue_com_commun/accesseur/AccesseurAchat.php");
+require_once (CHEMIN_SRC_DEV."microcabroue_com_commun/accesseur/AccesseurUtilisateur.php");
+require_once (CHEMIN_SRC_DEV."microcabroue_com_commun/accesseur/AccesseurEntiteGoodie.php");
+require_once (CHEMIN_SRC_DEV."microcabroue_com_commun/modele/Utilisateur.php");
+require_once (CHEMIN_SRC_DEV."microcabroue_com_commun/modele/Goodie.php");
 
-if(isset($_POST['bouton-rechercher']) && $_POST['bouton-rechercher'] == "rechercher")
+$accesseurGoodie = new AccesseurEntiteGoodie();
+$accesseurAchat = new AccesseurAchat();
+$accesseurUtilisateur = new AccesseurUtilisateur();
+afficherEntete($page);
+if(isset($_POST['bouton-choix']) && $_POST['bouton-choix'] == "selectionner")
 {
     switch($_POST['choix-recherche'])
     {
         case "numero-achat":
+            afficherParNumero($page);
+            break;
+        case "date":
+            break;
+        case "produit":
+            break;    
+        case "utilisateur":
+            break;
+    }
+}
+else
+{
+    afficherChoixDeRecherche($page);
+}
+
+if(isset($_POST['bouton-recherche']) && $_POST['bouton-recherche'] == "rechercher")
+{
+    switch($_POST['choix-recherche'])
+    {
+        case "numero-achat":
+            $page->resultatRecherche = [];
+            $achat = $accesseurAchat->rechercherParNumero($_POST['numero-achat']);
+            if(isset($achat->id_goodie) && is_int($achat->id_goodie))
+            {
+                $goodie = $accesseurGoodie->recupererGoodie($achat->id_goodie);
+                $utilisateur = $accesseurUtilisateur->recevoirUtilisateur($achat->id_utilisateur);
+                $achatAffichable = (object)
+                [
+                    "numero_achat" => $_POST['numero-achat'],
+                    "date" => $achat->date_achat,
+                    "produit" => $goodie->getNomFr(),
+                    "utilisateur" => $utilisateur->getPrenom()." ". $utilisateur->getNom(),
+                    "quantite" => $achat->quantite_produit 
+                ];     
+                $page->resultatRecherche[] = $achatAffichable;
+            }
             break;
         case "date":
             break;
@@ -15,5 +60,6 @@ if(isset($_POST['bouton-rechercher']) && $_POST['bouton-rechercher'] == "recherc
     }
 }
 
-afficherPage($page);
+
+afficherResultat($page);
 ?>
